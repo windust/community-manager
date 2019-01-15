cb27a900b8afcf3b1865febde853802894ae60a8
windust <freddy.guime@cjug.org>
2019-01-14 21:39:54 -0800

    Initial Setup

---
+++ src/main/java/com/spinningnoodle/communitymanager/communitymanager/CommunityManagerApplication.java
@@ -0,0 +1,14 @@
+package com.spinningnoodle.communitymanager.communitymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityManagerApplication.class, args);
+	}
+
+}
+
