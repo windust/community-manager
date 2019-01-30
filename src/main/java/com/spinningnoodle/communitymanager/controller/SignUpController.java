0d297e2c5ceab56a0a6c79315e7313b38e11c5e1
Troemer16 <troemer@mail.greenriver.edu>
2019-01-30 14:52:30 -0800

    fixed meetup details page to contain multiple venues

--- src/main/java/com/spinningnoodle/communitymanager/controller/SignUpController.java
+++ src/main/java/com/spinningnoodle/communitymanager/controller/SignUpController.java
@@ -1,5 +1,18 @@
 package com.spinningnoodle.communitymanager.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SignUpController {
+    @GetMapping("/venue")
+    public String venue(){
+        return "available_dates.html";
+    }
     
+    @GetMapping("/speaker")
+    public String speaker(){
+        return "";
+    }
 }
