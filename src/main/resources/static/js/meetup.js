47bf9bfa589cd65e7fa44af07ade2ef794237068
Jhakon Pappoe <jpappoe@mail.greenriver.edu>
2019-01-30 13:17:14 -0800

    updated javascript to hide the button when clicked and display a generated url.

--- src/main/resources/static/js/meetup.js
+++ src/main/resources/static/js/meetup.js
@@ -1,23 +1,19 @@
 function openTab(evt, category) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(category).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(category).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-  var hidden = false;
-  function hider(){
-      !hidden
-      if(hidden){
-          document.getElementById("request-host").style.visibility("hidden");
-      }else{
-        document.getElementById("request-host").style.visibility("visible");
-      }
-  }
+function hider() {
+    document.getElementById("request-host").style.display = 'none';
+    document.getElementById(
+        "generate-token").innerHTML = "<a href=\"http://www.google.com\">Click for your token</a>";
+}
