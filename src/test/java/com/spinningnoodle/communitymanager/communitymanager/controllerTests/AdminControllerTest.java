d186f756388aa5a01275065b8f18e69cac0817bc
Troemer16 <troemer@mail.greenriver.edu>
2019-02-04 13:37:06 -0800

    created tests for AdminController

---
+++ src/test/java/com/spinningnoodle/communitymanager/communitymanager/controllerTests/AdminControllerTest.java
@@ -0,0 +1,103 @@
+package com.spinningnoodle.communitymanager.communitymanager.controllerTests;
+
+import com.spinningnoodle.communitymanager.controller.AdminController;
+import com.spinningnoodle.communitymanager.exceptions.InvalidUserException;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AdminController.class)
+public class AdminControllerTest {
+    
+    @InjectMocks
+    private AdminController adminController;
+    
+    @BeforeEach
+    public void initializeController(){
+        adminController = new AdminController();
+    }
+    
+    @Test
+    public void loggedInStartsAsFalse(){
+        Assertions.assertEquals(ReflectionTestUtils.getField(adminController, "loggedIn"), false);
+    }
+    
+    @Test
+    public void loginReturnsLoginPage() {
+        Assertions.assertEquals("login", adminController.login());
+    }
+    
+    //TODO change from meetup to dashboard once dashboard page is complete
+    @Test
+    public void loginAttemptRedirectsToMeetupWhenSuccessful() {
+        Assertions.assertEquals("redirect:/meetup", adminController.loginAttempt("username", "password"));
+    }
+    
+    @Test
+    public void loginAttemptRedirectsToLoginWhenFailed(){
+        Assertions.assertEquals("redirect:/", adminController.loginAttempt("incorrect", "wrong"));
+    }
+    
+    @Test
+    public void loginAttemptSuccessful(){
+        adminController.loginAttempt("username", "password");
+        Assertions.assertEquals(ReflectionTestUtils.getField(adminController, "loggedIn"), true);
+    }
+    
+    @Test
+    public void loginAttemptFailed(){
+        adminController.loginAttempt("incorrect", "wrong");
+        Assertions.assertEquals(ReflectionTestUtils.getField(adminController, "loggedIn"), false);
+    }
+    
+    @Test
+    public void logoutRedirectsToLoginPage(){
+        Assertions.assertEquals("redirect:/", adminController.logout());
+    }
+    
+    @Test
+    public void logoutSetsLoggedInToFalse() {
+        ReflectionTestUtils.setField(adminController, "loggedIn", true);
+        adminController.logout();
+        Assertions.assertEquals(false, ReflectionTestUtils.getField(adminController, "loggedIn"));
+    }
+    
+    //TODO create dashboard tests
+//    @Test
+//    public void dashboard() {
+//    }
+    
+    @Test
+    public void meetupReturnsMeetupPageIfLoggedIn() throws InvalidUserException {
+        ReflectionTestUtils.setField(adminController, "loggedIn", true);
+        Assertions.assertEquals("meetup", adminController.meetup());
+    }
+    
+    @Test
+    public void meetupThrowsExceptionIfNotLoggedIn(){
+        ReflectionTestUtils.setField(adminController, "loggedIn", false);
+        Assertions.assertThrows(InvalidUserException.class, () -> adminController.meetup());
+    }
+    
+    @Test
+    public void getTokenThrowsExceptionIfNotLoggedIn(){
+        ReflectionTestUtils.setField(adminController, "loggedIn", false);
+        Assertions.assertThrows(InvalidUserException.class, () -> adminController.getToken());
+    }
+    
+    //TODO create token to retrieve from DB
+//    @Test
+//    public void getTokenReturnsTokenWhenLoggedIn() {
+//    }
+    
+    //TODO create google sheets Page
+//    @Test
+//    public void googleSheetsPage() {
+//    }
+}
