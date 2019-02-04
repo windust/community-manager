61b205b3221bc74bdf228c3099b790d1c365203d
kcbar <Kbarter@mail.greenriver.edu>
2019-02-04 14:20:20 -0800

    Unit tests for Token Generation to check and see if the Token is uniquie and not null

--- src/test/java/com/spinningnoodle/communitymanager/model/entities/TokenTest.java
+++ src/test/java/com/spinningnoodle/communitymanager/model/entities/TokenTest.java
@@ -1,5 +1,22 @@
 package com.spinningnoodle.communitymanager.model.entities;
 
-public class TokenTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class TokenTest {
+    private String nullValue = null;
+    private String valueInMap = "token1";
+
+    @Test
+    void tokenNotNull(){
+        String token = "Test value";
+        assertTrue(!token.equals(nullValue));
+    }
+
+    @Test
+    void uniqueToke(){
+        String token = "Test value";
+        assertTrue(!token.equals(valueInMap));
+    }
 }
