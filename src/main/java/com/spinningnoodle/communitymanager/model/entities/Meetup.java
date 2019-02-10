86e55f336e578eda5c856796e94d25dfbb53c163
Quentin Guenther <qguenther@outlook.com>
2019-02-10 00:14:18 -0800

    added class and function level javadoc to Meetup

--- src/main/java/com/spinningnoodle/communitymanager/model/entities/Meetup.java
+++ src/main/java/com/spinningnoodle/communitymanager/model/entities/Meetup.java
@@ -4,6 +4,14 @@ import com.spinningnoodle.communitymanager.exceptions.AttributeException;
 import com.spinningnoodle.communitymanager.exceptions.UnexpectedPrimaryKeyException;
 import java.util.Map;
 
+/**
+ * Meetup Entity
+ *
+ * Meetup stores all information associated with a meetup in the application
+ *
+ * @author Cream 4 UR Coffee
+ * @version 0.1
+ */
 public class Meetup implements IEntity {
 	private static int nextId = 1;
 
@@ -15,10 +23,23 @@ public class Meetup implements IEntity {
 	private String description;
 	private Venue venue;
 
+    /**
+     * Create a new Meetup with a unique generated object ID
+     */
 	public Meetup() {
 		setMeetupId();
 	}
 
+    /**
+     * Create a new Meetup with all fields and a unique generated ID
+     *
+     * @param primaryKey The key of the venue stored in the database
+     * @param date The date the meetup will be hosted
+     * @param speaker The speaker at the venue
+     * @param topic The topic of the meetup
+     * @param description The description of the meetup
+     * @param venue The venue which will host the venue
+     */
 	public Meetup(int primaryKey, String date, String speaker, String topic,
 		String description, Venue venue) {
 		this();
@@ -41,27 +62,32 @@ public class Meetup implements IEntity {
 		return this;
 	}
 
-	public static int getNextId() {
-		return nextId;
-	}
-
-	public static void setNextId(int nextId) {
-		Meetup.nextId = nextId;
-	}
-
+    /**
+     * @return This meetup's unique ID. NOT ASSOCIATED WITH PRIMARY KEY
+     */
 	public int getMeetupId() {
 		return meetupId;
 	}
 
+    /**
+     * Increments the Meetup's ID to the next available ID. NOT ASSOCIATED WITH PRIMARY KEY
+     */
 	public void setMeetupId() {
 		this.meetupId = nextId;
 		++nextId;
 	}
 
+    /**
+     * @return The meetup's primary key stored in DataStorage
+     */
 	public int getPrimaryKey() {
 		return primaryKey;
 	}
 
+    /**
+     * @param primaryKey The venue's primary key stored in DataStorage
+     * @throws UnexpectedPrimaryKeyException If Primary Key is 0 or < -1
+     */
 	public void setPrimaryKey(int primaryKey) {
 		if(primaryKey == 0 || primaryKey < -1) {
 			throw new UnexpectedPrimaryKeyException();
@@ -70,43 +96,86 @@ public class Meetup implements IEntity {
 		this.primaryKey = primaryKey;
 	}
 
+    /**
+     * @return The date the meetup will take place
+     */
 	public String getDate() {
 		return date;
 	}
 
+    /**
+     * @param date The date the meetup will take place
+     */
 	public void setDate(String date) {
 		this.date = date;
 	}
 
+    /**
+     * @return The speaker at the meetup
+     */
 	public String getSpeaker() {
 		return speaker;
 	}
 
+    /**
+     * @param speaker The speaker at the meetup
+     */
 	public void setSpeaker(String speaker) {
 		this.speaker = speaker;
 	}
 
+    /**
+     * @return The topic of the meetup
+     */
 	public String getTopic() {
 		return topic;
 	}
 
+    /**
+     * @param topic The topic of the meetup
+     */
 	public void setTopic(String topic) {
 		this.topic = topic;
 	}
 
+    /**
+     * @return The description of the meetup
+     */
 	public String getDescription() {
 		return description;
 	}
 
+    /**
+     * @param description The description of the meet up
+     */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+    /**
+     * @return The venue which will host the meetup
+     */
 	public Venue getVenue() {
 		return venue;
 	}
 
+    /**
+     * @param venue The venue which will host the meetup
+     */
 	public void setVenue(Venue venue) {
 		this.venue = venue;
 	}
+
+    @Override
+    public String toString() {
+        return "Meetup{" +
+            "meetupId=" + meetupId +
+            ", primaryKey=" + primaryKey +
+            ", date='" + date + '\'' +
+            ", speaker='" + speaker + '\'' +
+            ", topic='" + topic + '\'' +
+            ", description='" + description + '\'' +
+            ", venue=" + venue +
+            '}';
+    }
 }
