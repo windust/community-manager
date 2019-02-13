deb3fa84a372bf51ce48cee47e0ae40597e9ce5d
QuentinGuenther <qguenther@outlook.com>
2019-02-13 00:05:09 -0800

    create observer/observable files

--- src/main/java/com/spinningnoodle/communitymanager/model/observer/Observable.java
+++ src/main/java/com/spinningnoodle/communitymanager/model/observer/Observable.java
@@ -1,41 +1,5 @@
 package com.spinningnoodle.communitymanager.model.observer;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * defines an abstract interface with the callback operation IObserver objects use to notify observers.
- *
- * @author Cream 4 UR Coffee
- * @version 0.1
- */
 public abstract class Observable {
-	private List<IObserver> observers = new ArrayList<>();
 
-	/**
-	 * Attach a observer to this object
-	 *
-	 * @param observer A class which listens to updates in this class
-	 */
-	public void atachObserver(IObserver observer) {
-		observers.add(observer);
-	}
-
-	/**
-	 * Detach a observer to this object
-	 *
-	 * @param observer A class which listens to updates in this class
-	 */
-	public void detachObserver(IObserver observer) {
-		observers.remove(observer);
-	}
-
-	/**
-	 * Call update method on all observers to let them know a change in state has been made.
-	 */
-	protected void notifyObservers() {
-		for(IObserver observer : observers) {
-			observer.update(this);
-		}
-	}
 }
