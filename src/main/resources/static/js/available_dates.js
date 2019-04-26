17accebf1a5f7c39a9d4c587f6aa66b72350a18f
Mels Ramblings <mfelton@mail.greenriver.edu>
2019-04-26 14:12:08 -0700

    refactored javascript function so less repeated code.

--- src/main/resources/static/js/available_dates.js
+++ src/main/resources/static/js/available_dates.js
@@ -9,24 +9,16 @@
  *
  *  END OF LICENSE INFORMATION
  */
-// document.forms["meetup_signup","meetup_requested","meetup_confirm"].onsubmit = function (){
-document.forms["meetup_signup"].onsubmit = function (){
-  return confirmation();
-}
 
-document.forms["meetup_requested"].onsubmit = function (){
-  return confirmation();
-}
-
-document.forms["meetup_confirm"].onsubmit = function (){
-  return confirmation();
+for(i = 0; i < document.forms.length;i++){
+  document.forms[i].onsubmit = function (){
+    return confirmation();
+  }
 }
 
 confirmation = function (){
   var thisButton = document.activeElement;
   var hiddenInput = document.getElementById("hiddenInput");
-  var modalYesButton = document.getElementById("modalYes");
-  var modalNoButton = document.getElementById("modalNo");
 
   var date = thisButton.value;
   var hiddenValue = hiddenInput.value;
@@ -35,65 +27,48 @@ confirmation = function (){
     if (date === "notHosting" && hiddenValue === "unused") {
       return true;
     }
-
-    hiddenInput.value = "confirm";
     hiddenInput.name = "confirm";
-    openVenueConfirmModal(thisButton);
+    openVenueConfirmModal();
     return false;
   }
 
   if(hiddenInput.name == "confirm") {
     if (date == "notHosting") {
-      closeModal(thisButton);
       resetModal();
       return false;
     }
-
-    openFoodConfirmModal(thisButton);
+    openFoodConfirmModal();
     hiddenInput.name = "meetup";
     hiddenInput.value = date;
     return false;
   }
 
   if(hiddenValue == "meetup") {
-
-      closeModal(thisButton);
       return true;
-
   }
 }
 
-
-openVenueConfirmModal = function (element){
+openConfirmModal = function (confirmType, confirmMessage) {
   modal = document.getElementById("modal");
   modal.classList.remove("hidden");
+
   var date = document.activeElement.value;
-
-  document.getElementById("modalYes").name = "meetup";
-  document.getElementById("modalNo").name = "meetup";
   document.getElementById("modalYes").value = date;
-
   document.getElementById("modalDate").innerHTML = date;
-  document.getElementById("modalMessage").innerHTML = "are you sure you want to host on";
+
+  document.getElementById("modalYes").name = confirmType;
+  document.getElementById("modalNo").name = confirmType;
+
+  document.getElementById("modalMessage").innerHTML = confirmMessage;
 }
 
-openFoodConfirmModal = function (element) {
-  modal = document.getElementById("modal");
-  modal.classList.remove("hidden");
+openVenueConfirmModal = function (){
+  openConfirmModal("meetup", "are you sure you want to host on");
+}
+
+openFoodConfirmModal = function () {
+  openConfirmModal("food", "can you provide food on");
   modal.classList.add("foodModal");
-  var date = document.activeElement.value;
-
-  document.getElementById("modalYes").name = "food";
-  document.getElementById("modalNo").name = "food";
-  document.getElementById("modalYes").value = date;
-
-  document.getElementById("modalDate").innerHTML = date;
-  document.getElementById("modalMessage").innerHTML = "can you provide food on";
-}
-
-confirmFood = function (element, foodDecision) {
-  var date = document.activeElement.value;
-  window.location.href = "/venueSignUp?meetup="+date+"&food=" + foodDecision;
 }
 
 resetModal = function () {
@@ -106,10 +81,7 @@ resetModal = function () {
 
   modalYesButton.name = "meetup";
   modalNoButton.name = "meetup";
-}
 
-closeModal = function (element) {
-  modal = document.getElementById("modal");
+  modal.classList.remove("foodModal");
   modal.classList.add("hidden");
-
 }
