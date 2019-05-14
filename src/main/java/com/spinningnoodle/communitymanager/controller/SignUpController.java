e1ba8784867f974f49d9255d217a32f170e331c4
Troemer16 <troemer@mail.greenriver.edu>
2019-05-14 10:47:41 -0700

    Removed debugging print statement

--- src/main/java/com/spinningnoodle/communitymanager/controller/SignUpController.java
+++ src/main/java/com/spinningnoodle/communitymanager/controller/SignUpController.java
@@ -23,7 +23,6 @@ import java.time.LocalDate;
 import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -110,7 +109,7 @@ public class SignUpController {
                 currentMeetup = meetup;
             }
         }
-        System.out.println(meetups.get(0).getVenueEntity().getAddress());
+        
         session.setAttribute("meetups", meetups);
         session.setAttribute(responderType, responder);
         session.setAttribute("hostingMessage", model.getMessage(responder));
