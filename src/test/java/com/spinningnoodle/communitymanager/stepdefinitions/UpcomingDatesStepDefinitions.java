0292102ec656b609bd78c34ebefb95e5919321bb
Quentin Guenther <qguenther@outlook.com>
2019-05-16 14:49:02 -0700

    behavior test accordion elements

--- src/test/java/com/spinningnoodle/communitymanager/stepdefinitions/UpcomingDatesStepDefinitions.java
+++ src/test/java/com/spinningnoodle/communitymanager/stepdefinitions/UpcomingDatesStepDefinitions.java
@@ -1,25 +1,23 @@
 package com.spinningnoodle.communitymanager.stepdefinitions;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.spinningnoodle.communitymanager.AbstractDefs;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.io.UnsupportedEncodingException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
 public class UpcomingDatesStepDefinitions extends AbstractDefs {
-    
-    @Given("The client is signed in")
-    public void theClientSignsIn() {
-        
-    }
-
     @When("^the client calls /upcoming$")
     public void theClientCallsUpcoming() throws Throwable {
         this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
@@ -28,13 +26,34 @@ public class UpcomingDatesStepDefinitions extends AbstractDefs {
 
     @Then("^the client receives status code of (\\d+)$")
     public void theClientReceivesStatusCodeOf(int statusCode) throws Throwable {
-        mvc.perform(get("/upcoming"))
-            .andExpect(status().is(statusCode))
-            .andExpect(content().contentType("application/hal+json"));
+        assertEquals(statusCode, result.getResponse().getStatus());
     }
 
-    @And("^the client receives server version (.+)$")
-    public void theClientReceivesServerVersion(String version) throws Throwable {
-        //assertThat(latestResponse.getBody(), is(version)) ;
+    @And("^the response is text/html$")
+    public void theResponseIsTextHtml() {
+        assertEquals("text/html;charset=UTF-8", result.getResponse().getContentType());
+    }
+
+    @Then("^the accordion is made of summary tags$")
+    public void theAccordianIsMadeOfSummaryTags() throws UnsupportedEncodingException {
+        Document doc = Jsoup.parse(result.getResponse().getContentAsString());
+
+        assertTrue(doc.getElementsByTag("summary").size() > 0);
+    }
+
+    @And("^the data in the acordion is not default$")
+    public void theDataInTheAccordionIsNotDefault() throws UnsupportedEncodingException {
+        Document doc = Jsoup.parse(result.getResponse().getContentAsString());
+
+        assertAll("", () -> {
+            for(Element fold : doc.getElementsByTag("summary")) {
+                for(Element header : fold.children()) {
+                    assertNotEquals("Date", header.text());
+                    assertNotEquals("Speaker", header.text());
+                    assertNotEquals("Venue", header.text());
+                }
+
+            }
+        });
     }
 }
--- src/test/resources/upcoming_dates.feature
+++ src/test/resources/upcoming_dates.feature
@@ -2,4 +2,8 @@ Feature: The Upcoming Dates page can be accessed
   Scenario: Client makes call to GET /upcoming
     When the client calls /upcoming
     Then the client receives status code of 200
-    And the client receives server version 1.0
+    And the response is text/html
+  Scenario: Client reads the page
+    When the client calls /upcoming
+    Then the accordion is made of summary tags
+    And the data in the accordion is not default
